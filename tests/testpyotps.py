f46910ee81eb659dffb64d175bdb18c2b2f8d39a
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-06-08 19:08:43 -0400

    Simplifies test

--- tests/testpyotps.py
+++ tests/testpyotps.py
@@ -1,11 +1,7 @@
 import sys; sys.path.append('../pyotps')
 import pyotps
 
-otpspath_nc = '/data5/glwagner/Numerics/nestgcm/pyotps/OTPSnc'
-otpspath_v1 = '/data5/glwagner/Numerics/nestgcm/pyotps/OTPS2'
+otpspath = '/data5/glwagner/Numerics/patches/pyotps/OTPS2'
 
-driver = pyotps.tidaldriver(otpspath=otpspath_nc, modeltype='netcdf')
-driver.testdrive()
-
-driver = pyotps.tidaldriver(otpspath=otpspath_v1, modeltype='v1')
+driver = pyotps.tidaldriver(otpspath=otpspath, modeltype='v1')
 driver.testdrive()
